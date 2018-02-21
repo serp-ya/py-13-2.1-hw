8edbf40ccbc96718e14b17ce15dd164cffe0193f
Sergey G. Kundryukov <skundryukov@tsc.ts>
2018-02-21 13:55:32 +0300

    Добавлена запись в файл products_order_list.txt

--- core.py
+++ core.py
@@ -1,25 +1,48 @@
+from datetime import datetime
+
 def get_shop_list_by_dishes(dishes, person_count, cook_book):
-  shop_list = {}
-  for dish in dishes:
-    for ingridient in cook_book[dish]:
-      new_shop_list_item = dict(ingridient)
+    shop_list = {}
+    for dish in dishes:
+        for ingridient in cook_book[dish]:
+            new_shop_list_item = dict(ingridient)
 
-      new_shop_list_item['quantity'] *= person_count
-      if new_shop_list_item['ingridient_name'] not in shop_list:
-        shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
-      else:
-        shop_list[new_shop_list_item['ingridient_name']]['quantity'] += \
-          new_shop_list_item['quantity']
-  return shop_list
+            new_shop_list_item['quantity'] *= person_count
+            if new_shop_list_item['ingridient_name'] not in shop_list:
+                shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
+            else:
+                shop_list[new_shop_list_item['ingridient_name']]['quantity'] += \
+                    new_shop_list_item['quantity']
+    return shop_list
 
-def print_shop_list(shop_list):
-  for shop_list_item in shop_list.values():
-    print('{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['quantity'],
-                            shop_list_item['measure']))
+
+def formated_list_item(list_item):
+    return '{} {} {}'.format(list_item['ingridient_name'], list_item['quantity'], list_item['measure'])
+
+def go_over_list(list, callback):
+    for list_item in list.values():
+        callback(list_item)
+
+
+def print_shop_list(list_item):
+    print(formated_list_item(list_item))
+
+
+def write_products_order_list(list_item):
+    with open('products_order_list.txt', encoding='utf8', mode='a') as order_list:
+        order_list.write(formated_list_item(list_item))
+        order_list.write('\n')
+
 
 def create_shop_list(cook_book):
-  person_count = int(input('Введите количество человек: '))
-  dishes = input('Введите блюда в расчете на одного человека (через запятую): ') \
-    .lower().split(', ')
-  shop_list = get_shop_list_by_dishes(dishes, person_count, cook_book)
-  print_shop_list(shop_list)
+    person_count = int(input('Введите количество человек: '))
+    dishes = input('Введите блюда в расчете на одного человека (через запятую): ') \
+        .lower().split(', ')
+    shop_list = get_shop_list_by_dishes(dishes, person_count, cook_book)
+
+    go_over_list(shop_list, print_shop_list)
+
+    with open('products_order_list.txt', encoding='utf8', mode='w') as order_list:
+        order_list.write(str(datetime.now()))
+        order_list.write('\n')
+
+    go_over_list(shop_list, write_products_order_list)
