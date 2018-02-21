c2d2ee84bea79b991b79f8c211a3cd8fb4826f80
Sergey G. Kundryukov <skundryukov@tsc.ts>
2018-02-21 13:18:03 +0300

    Начальный коммит

---
+++ core.py
@@ -0,0 +1,25 @@
+def get_shop_list_by_dishes(dishes, person_count, cook_book):
+  shop_list = {}
+  for dish in dishes:
+    for ingridient in cook_book[dish]:
+      new_shop_list_item = dict(ingridient)
+
+      new_shop_list_item['quantity'] *= person_count
+      if new_shop_list_item['ingridient_name'] not in shop_list:
+        shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
+      else:
+        shop_list[new_shop_list_item['ingridient_name']]['quantity'] += \
+          new_shop_list_item['quantity']
+  return shop_list
+
+def print_shop_list(shop_list):
+  for shop_list_item in shop_list.values():
+    print('{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['quantity'],
+                            shop_list_item['measure']))
+
+def create_shop_list(cook_book):
+  person_count = int(input('Введите количество человек: '))
+  dishes = input('Введите блюда в расчете на одного человека (через запятую): ') \
+    .lower().split(', ')
+  shop_list = get_shop_list_by_dishes(dishes, person_count, cook_book)
+  print_shop_list(shop_list)
